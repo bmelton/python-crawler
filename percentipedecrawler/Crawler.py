2765e131eaf5947403a588d11d4e61e7f2f4a706
Barry Melton <barry.melton@gmail.com>
2023-02-25 16:38:45 -0500

    Fucking ell

--- percentipedecrawler/Crawler.py
+++ percentipedecrawler/Crawler.py
@@ -4,12 +4,38 @@ import requests
 from lxml import etree
 import urllib.robotparser
 from time import time
-from save import save
 from setup import setup
 from nanoid import generate
-import sys
+from sqlalchemy.orm import Session
+from database import get_engine
+from models import Crawls
+import markdownify
+from bs4 import BeautifulSoup
+from models import metadata
+from database import get_engine
 
-sys.path.append(".")
+
+def setup():
+    engine = get_engine()
+    metadata.create_all(bind=engine)
+
+
+def strip_tags(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    head = soup.find("head")
+    head.clear()
+    return soup.body.get_text()
+
+
+def save(crawl_id, domain, url, html):
+    engine = get_engine()
+    with Session(engine) as session:
+        text = strip_tags(html)
+        md = markdownify.markdownify(text, heading_style="ATX")
+        crawl = Crawls(crawl_id=crawl_id, domain=domain, url=url,
+                       html=html, markdown=md, text=text)
+        session.add(crawl)
+        return session.commit()
 
 
 class Crawler:
